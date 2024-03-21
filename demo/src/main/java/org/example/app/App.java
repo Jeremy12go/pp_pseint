6e2c300b81b29b3781b588ba3c16c2e1a0da2e5b
Jeremi Arriagada <jeremy12go@gmail.com>
2024-03-21 20:45:04 -0300

    Diseño del panel de trabajo, con funcion ajustable, aplicable posteriormente a las demas secciones

--- demo/src/main/java/org/example/app/App.java
+++ demo/src/main/java/org/example/app/App.java
@@ -3,9 +3,11 @@ import Clases.Diagrama;
 import Clases.Persistencia;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
@@ -15,11 +17,31 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("interfaz.fxml"));
-        scene = new Scene(fxmlLoader.load(), 900, 600);
+        Parent root = fxmlLoader.load();
+        AppController appController = fxmlLoader.getController();
+
+        //Dimensiones panel inicial
+        double initialWidth = 740;
+        double initialHeight = 1500;
+        appController.ajustar_ScrollPane(initialWidth, initialHeight);
+
         Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("Icono_PP_white.png")));
+
+        stage.maximizedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+                double screenWidth = screenBounds.getWidth();
+                double screenHeight = screenBounds.getHeight();
+                appController.ajustar_ScrollPane(screenWidth, screenHeight);
+                appController.rellenarImagen();
+            } else {
+                appController.ajustar_ScrollPane(740, 1500);
+            }
+        });
+
         stage.getIcons().add(image);
         stage.setTitle("DiagramFlex");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
--- demo/src/main/java/org/example/app/AppController.java
+++ demo/src/main/java/org/example/app/AppController.java
@@ -1,14 +1,56 @@
 package org.example.app;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import java.io.IOException;
 
 public class AppController {
     @FXML
-    private Label welcomeText;
+    AnchorPane panel_Diagrama;
+    @FXML
+    ScrollPane panel_contenedor;
+    @FXML
+    public void initialize() throws IOException {
+        fondoCuadriculado();
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-
+    protected void fondoCuadriculado() throws IOException {
+        Image image = new Image("C:\\Users\\jerem\\OneDrive\\Documents\\Cosas\\Scripts Java\\Proyecto-PSEint\\demo\\src\\main\\resources\\org\\example\\app\\fondoCuadriculado.jpg");
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+        panel_Diagrama.setBackground(background);
+        panel_Diagrama.setMinSize(740, 1500);
     }
+    public void ajustar_ScrollPane(double ancho, double largo){
+        panel_contenedor.setMinSize(ancho,largo);
+    }
+    public void rellenarImagen() {
+        double scrollPaneWidth = panel_contenedor.getWidth();
+        double scrollPaneHeight = panel_contenedor.getHeight();
+
+        // Configura la imagen para mantener su ascpecto luego del ajuste al tamaño(En proceso...)
+        if (scrollPaneWidth > scrollPaneHeight) {
+            panel_Diagrama.getChildren().forEach(node -> {
+                if (node instanceof ImageView) {
+                    ImageView imageView = (ImageView) node;
+                    imageView.setFitWidth(scrollPaneWidth);
+                    imageView.setFitHeight(-1);
+                }
+            });
+        } else {
+            panel_Diagrama.getChildren().forEach(node -> {
+                if (node instanceof ImageView) {
+                    ImageView imageView = (ImageView) node;
+                    imageView.setFitWidth(-1);
+                    imageView.setFitHeight(scrollPaneHeight);
+                }
+            });
+        }
+    }
+
+
 }
