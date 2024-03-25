2155bdaf6136849b6aab5f110da77734a93bffdd
Renatto Navarro <renatto.nb2003@gmail.com>
2024-03-24 22:59:55 -0300

    Agregar imagen al menú de opciones y agregar movimiento.

--- demo/src/main/java/org/example/app/AppController.java
+++ demo/src/main/java/org/example/app/AppController.java
@@ -5,6 +5,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import java.io.IOException;
+import javafx.scene.input.MouseEvent;
 
 public class AppController {
     @FXML
@@ -12,19 +13,72 @@ public class AppController {
     @FXML
     ScrollPane panel_contenedor;
     @FXML
+    private double initialX;
+    @FXML
+    private double initialY;
+
+    @FXML
     public void initialize() throws IOException {
         fondoCuadriculado();
+        Image image1 = new Image("C:\\Users\\Renatto\\IdeaProjects\\pp_pseint\\demo\\src\\main\\resources\\org\\example\\app\\diagrama_1.png");
+        diagrama_1.setImage(image1);
+        Image image2 = new Image("C:\\Users\\Renatto\\IdeaProjects\\pp_pseint\\demo\\src\\main\\resources\\org\\example\\app\\diagrama_2.png");
+        diagrama_2.setImage(image2);
+        Image image3 = new Image("C:\\Users\\Renatto\\IdeaProjects\\pp_pseint\\demo\\src\\main\\resources\\org\\example\\app\\diagrama_3.png");
+        diagrama_3.setImage(image3);
+        Image image4 = new Image("C:\\Users\\Renatto\\IdeaProjects\\pp_pseint\\demo\\src\\main\\resources\\org\\example\\app\\diagrama_4.png");
+        diagrama_4.setImage(image4);
+    }
+
+    //--------------------------------------------------
+    @FXML
+    private ImageView diagrama_1;
+    @FXML
+    private ImageView diagrama_2;
+    @FXML
+    private ImageView diagrama_3;
+    @FXML
+    private ImageView diagrama_4;
+
+    @FXML
+    private void onMousePressed(MouseEvent event) {
+        initialX = event.getSceneX();
+        initialY = event.getSceneY();
     }
 
     @FXML
+    private void onMouseDragged(MouseEvent event) {
+
+        double deltaX = event.getSceneX() - initialX;
+        double deltaY = event.getSceneY() - initialY;
+
+        //solucionar
+        diagrama_1.setLayoutX(diagrama_1.getLayoutX() + deltaX);
+        diagrama_1.setLayoutY(diagrama_1.getLayoutY() + deltaY);
+
+        initialX = event.getSceneX();
+        initialY = event.getSceneY();
+    }
+
+    @FXML
+    private void onMouseReleased(MouseEvent event) {
+        // Agregar accciones adicionales
+    }
+
+    //--------------------------------------------------
+
+    @FXML
     protected void fondoCuadriculado() {
-        Image image = new Image("C:\\Users\\jerem\\OneDrive\\Documents\\Cosas\\Scripts Java\\Proyecto-PSEint\\demo\\src\\main\\resources\\org\\example\\app\\fondoCuadriculado.jpg");
+        Image image = new Image("C:\\Users\\Renatto\\IdeaProjects\\pp_pseint\\demo\\src\\main\\resources\\org\\example\\app\\fondoCuadriculado.jpg");
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
         panel_Diagrama.setBackground(background);
         panel_Diagrama.setMinSize(740, 1500);
     }
+
+
+
     public void ajustar_ScrollPane(double ancho, double largo){
         panel_contenedor.setMinSize(ancho,largo);
     }
