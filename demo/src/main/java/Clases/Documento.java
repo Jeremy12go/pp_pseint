817e523f18d7ada50e986486a7e7a83c3ab6951f
Jeremi Arriagada <jeremy12go@gmail.com>
2024-04-19 00:04:16 -0400

    Agregación de instancia de Figura documento y parametros para la conexion al diagrama

---
+++ demo/src/main/java/Clases/Documento.java
@@ -0,0 +1,8 @@
+package Clases;
+
+public class Documento extends Figura{
+
+    public Documento(String contenido, Vertice vertice_cordenada, Vertice vertice_conexion, Arista dimenciones) {
+        super(contenido, vertice_cordenada, vertice_conexion, dimenciones);
+    }
+}
