752ad1dbb44c7e897e0619717ea403d58379ba87
Jeremy12go <jeremy12go@gmail.com>
2024-05-30 18:21:52 -0400

    Conjunción de avances de semanas anteriores con version sin terminar de conexiones de igual dimension

---
+++ demo/src/main/java/Clases/VG.java
@@ -0,0 +1,28 @@
+package Clases;
+
+import javafx.scene.canvas.Canvas;
+
+public class VG {
+    private static Figura ultimaFiguraAñadida;
+    private static Canvas ultimoCanvasFigura;
+    private static Canvas ultimoCanvasConexion;
+
+    public static void cambiarUltimaFiguraAñadida(Figura f) {
+        ultimaFiguraAñadida = f;
+    }
+    public static void cambiarUltimoCanvasFigura(Canvas c) {
+        ultimoCanvasFigura = c;
+    }
+    public static void cambiarUltimoCanvasConexion(Canvas c) {
+        ultimoCanvasConexion = c;
+    }
+    public static Figura getUltimaFiguraAñadida() {
+        return ultimaFiguraAñadida;
+    }
+    public static Canvas getUltimoCanvasFigura() {
+        return ultimoCanvasFigura;
+    }
+    public static Canvas getUltimoCanvasConexion() {
+        return ultimoCanvasConexion;
+    }
+}
